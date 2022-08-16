ec1393fdcaf576bc83288bf8110d4188b339efc1
Hewerton Moreira <hewertonfm@gmail.com>
2022-08-16 08:30:04 -0300

    adjust controller

--- Fipe/Controllers/FipeWSController.cs
+++ Fipe/Controllers/FipeWSController.cs
@@ -13,13 +13,13 @@ namespace Fipe.Controllers
         /// <param name="fipeCode">Car FIPE Code</param>
         /// <returns>A car value</returns>
         [HttpGet("{fipeCode}")]
-        public IActionResult Get(string fipeCode)
+        public IActionResult Get([FromRoute] string fipeCode)
         {
             try
             {
-            var fipeWS = new FipeWS();
-            var result = fipeWS.GetFipeInfo(fipeCode);
-            return Ok(result.VehiclePrice);
+                var fipeWS = new FipeWS();
+                var result = fipeWS.GetFipeInfo(fipeCode);
+                return Ok(result.VehiclePrice);
             }
             catch
             {
